400e20785b92d781b3f1198de1c075c83b5e60eb
Wil Alvarez <wil_alejandro@yahoo.com>
2011-03-04 18:13:35 -0430

    Optimizado el módulo de notificaciones

--- turpial/notification.py
+++ turpial/notification.py
@@ -20,57 +20,69 @@ except ImportError:
 class Notification:
     """Manejo de notificaciones"""
     def __init__(self, disable):
-        self.activate()
-        self.disable = disable
         if disable:
+            self.deactivate()
             log.debug('Módulo deshabilitado')
+        else:
+            self.activate()
+        iconpath = os.path.join(os.path.dirname(__file__), 'data', 'pixmaps', 
+            'turpial-notification.png')
+        self.default_icon = os.path.realpath(iconpath)
+    
+    def __popup(self, title, message, icon=None):
+        if self.disable:
+            log.debug('Módulo deshabilitado. No hay notificaciones')
+            return
         
+        if not self.active or not NOTIFY:
+            return
+        
+        try:
+            if pynotify.init("Turpial"):
+                if not icon:
+                    icon = self.default_icon
+                icon = "file://%s" % icon
+                notification = pynotify.Notification(title, message, icon)
+                notification.show()
+        except Exception, e:
+            log.debug('Error en notificacion: %s' % e)
+    
     def toggle_activation(self):
         if self.active:
             self.active = False
         else:
             self.active = True
-
+    
     def activate(self):
         self.active = True
         
     def deactivate(self):
         self.active = False
-        
-    def popup(self, title, message, icon=None):
-        if self.disable:
-            log.debug('Módulo deshabilitado. No hay notificaciones')
-            return
-            
-        if self.active and NOTIFY:
-            if pynotify.init("Turpial"):
-                if not icon:
-                    iconpath = os.path.join(os.path.dirname(__file__), 'data', 
-                        'pixmaps', 'turpial-notification.png')
-                    icon = os.path.realpath(iconpath)
-                icon = "file://%s" % icon
-                notification = pynotify.Notification(title, message, icon)
-                notification.show()
     
     def new_tweets(self, title, count, tobject, tweet, icon):
-        self.popup('%s (%i %s)' % (title, count, tobject), tweet, icon)
+        self.__popup('%s (%i %s)' % (title, count, tobject), tweet, icon)
         
     def login(self, p):
-        self.popup('@%s' % p.username,
+        self.__popup('@%s' % p.username,
             '%s: %i\n%s: %i\n%s: %i' % 
             (_('Tweets'), p.statuses_count,
             _('Following'), p.friends_count, 
             _('Followers'), p.followers_count))
     
+    def login_error(self, message):
+        self.__popup(_('Authentication error'), message)
+        
     def following(self, user, follow):
         name = user.username
         if follow:
-            self.popup(_('Turpial (Follow)'), _('You are now following @%s') % name)
+            self.__popup(_('Turpial (Follow)'), 
+                _('You are now following @%s') % name)
         else:
-            self.popup(_('Turpial (Unfollow)'), _('You are no longer following @%s') % name)
+            self.__popup(_('Turpial (Unfollow)'), 
+                _('You are no longer following @%s') % name)
                 
     def following_error(self, message, follow):
         if follow:
-            self.popup(_('Turpial (Follow)'), message)
+            self.__popup(_('Turpial (Follow)'), message)
         else:
-            self.popup(_('Turpial (Unfollow)'), message)
+            self.__popup(_('Turpial (Unfollow)'), message)
